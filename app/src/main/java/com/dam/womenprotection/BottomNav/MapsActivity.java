8fbe35f608234e1ee0bbfdbd667a570890085a62
LidiaParral <72860314+LidiaParral@users.noreply.github.com>
2021-04-28 20:48:20 +0200

    Update

--- app/src/main/java/com/dam/womenprotection/BottomNav/MapsActivity.java
+++ app/src/main/java/com/dam/womenprotection/BottomNav/MapsActivity.java
@@ -1,28 +1,40 @@
 package com.dam.womenprotection.BottomNav;
 
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.dam.womenprotection.R;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
-    Button btnSatelite;
+    private Marker marcardor;
+    double lat = 0.0;
+    double lon = 0.0;
 
 
     @Override
@@ -32,6 +44,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         navigationView.setSelectedItemId(R.id.Maps);
 
@@ -58,19 +73,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return false;
         });
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
-        btnSatelite = findViewById(R.id.btnSatelite);
 
-        btnSatelite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
-            }
-        });
     }
+
 
 
     @Override
@@ -78,26 +85,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         map = googleMap;
 
-        Antut(googleMap);
+        ubicacionActual();
 
     }
 
-    private void Antut(GoogleMap googleMap) {
-        map = googleMap;
+    private void agregarMarcador(double lat, double lon) {
+        LatLng coordenadas = new LatLng(lat, lon);
+        CameraUpdate ubicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
+        if (marcardor != null) {
+            marcardor.remove();
+        }
+
+        marcardor = map.addMarker(new MarkerOptions()
+                .position(coordenadas)
+                .title("Ubicación Actual")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ubica)));
+
+        map.animateCamera(ubicacion);
 
 
-        final LatLng punto2 = new LatLng(25.4190531, -3.69361943);
-        map.addMarker(new MarkerOptions().position(punto2).title("Punto 2").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            actualizarUbicacion(location);
+        }
+    };
+
+    private void ubicacionActual() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,15000,0,locationListener);
+
     }
 
 
-    public void buscar(View view) {
+    private void actualizarUbicacion(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+            agregarMarcador(lat, lon);
+        }
 
-        LatLng LatLng = new LatLng(40.4190531, -3.69361943);
-        map.addMarker(new MarkerOptions().position(LatLng).title("Madrid-España").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng,20));
-        map.getFocusedBuilding();
-        map.setTrafficEnabled(true);
-        map.setBuildingsEnabled(true);
     }
+
+
 }
